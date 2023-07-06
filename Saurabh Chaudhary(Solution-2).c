c82489cf6b8d0588327c21cceeacf262b3eaec11
saurabh chuadhary <supersaurabh017@gmail.com>
2023-07-06 19:20:55 +0530

    solutions

--- Saurabh Chaudhary(Solution-2).c
+++ Saurabh Chaudhary(Solution-2).c
@@ -1,44 +1,110 @@
+/*
+Date String to Structure conversion
+In a application, the date is stored as a string and for computations, converting the string to number is required.
+Write a function to convert the string formated date to a structure as per the below code
+Return appropriate error values if the conversion is not successful
+
+Inptu string format
+DD/MM/YYYY
+
+Output Structure format
+typedef struct my_date_t
+{
+    uint8_t date; //date
+    uint8_t month; //month
+    uint16_t year; //year
+}my_date_t;
+
+Conversion status
+typedef enum status_t
+{
+    SUCCESS,                    //< Function has successfully converted the string to structure
+    NULL_PTR,                   //< Function is given NULL pointers to work with
+    INCORRECT                   //< Incorrect values for date or month or year
+}status_t;
+
+
+Function prototype
+@brief Function to convert string of the format DD/MM/YYYY to my_date_t structure
+@param input_string String which needs to be converted
+@param result_time Resultant structure to hold the converted string
+@return status_t SUCCESS if conversion is done, status_t code otherwise.
+status_t string_to_date_converter(char* input_string, my_date_t* result_date);
+*/
+
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 
 typedef struct my_date_t
 {
-    uint8_t date;
-    uint8_t month;
-    uint16_t year;
+   uint8_t date;  // date
+   uint8_t month; // month
+   uint16_t year; // year
 } my_date_t;
 
-int parseDateString(const char* dateString, my_date_t* result) {
-    if (dateString == NULL || result == NULL || strlen(dateString) != 10)
-        return -1;
+typedef enum status_t
+{
+   SUCCESS,  // Function has successfully converted the string to structure
+   NULL_PTR, // Function is given NULL pointers to work with
+   INCORRECT // Incorrect values for date or month or year
+} status_t;
 
-    char dayStr[3], monthStr[3], yearStr[5];
-    sscanf(dateString, "%2[^/]/%2[^/]/%4s", dayStr, monthStr, yearStr);
-    
+status_t string_to_date_converter(char *input_string, my_date_t *result_date)
+{
+   if (input_string == NULL || result_date == NULL)
+   {
+      return NULL_PTR;
+   }
 
-    int day = atoi(dayStr);
-    int month = atoi(monthStr);
-    int year = atoi(yearStr);
+   int day, month, year;
+   if (sscanf(input_string, "%d/%d/%d", &day, &month, &year) != 3)
+   {
+      return INCORRECT;
+   }
 
-    result->date = (uint8_t)day;
-    result->month = (uint8_t)month;
-    result->year = (uint16_t)year;
-    
-    return 0;
+   if (day < 1 || day > 31 || month < 1 || month > 12)
+   {
+      return INCORRECT;
+   }
+
+   result_date->date = (uint8_t)day;
+   result_date->month = (uint8_t)month;
+   result_date->year = (uint16_t)year;
+
+   return SUCCESS;
 }
 
-int main() {
-     char dateString[12];
-    fgets(dateString, sizeof(dateString), stdin);
-    my_date_t result;
-    
-    if (0==parseDateString(dateString, &result)) {
-        printf("Date: %02u/%02u/%04u\n", result.date, result.month, result.year);
-    } else {
-        printf("Failed to parse date string.\n");
-    }
-    
-    return 0;
+int main()
+{
+   char input_string[12]; // Max length for "DD/MM/YYYY" is 10 characters, plus 1 for null terminator
+   printf("Enter a date (DD/MM/YYYY): ");
+   fgets(input_string, sizeof(input_string), stdin);
+
+   if (input_string[strlen(input_string) - 1] == '\n') // Remove the trailing newline character from the input string
+   {
+      input_string[strlen(input_string) - 1] = '\0';
+   }
+
+   my_date_t result_date;
+   status_t conversion_status = string_to_date_converter(input_string, &result_date);
+
+   if (conversion_status == SUCCESS)
+   {
+      printf("Conversion successful\n");
+      printf("Date: %d\n", result_date.date);
+      printf("Month: %d\n", result_date.month);
+      printf("Year: %d\n", result_date.year);
+   }
+   else if (conversion_status == NULL_PTR)
+   {
+      printf("Error: NULL pointers\n");
+   }
+   else if (conversion_status == INCORRECT)
+   {
+      printf("Error: Incorrect date format\n");
+   }
+
+   return 0;
 }
